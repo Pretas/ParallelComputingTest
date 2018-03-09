46ee5ff5e54c64e53a31ed5a0cf3b3d0b162f2d0
DESKTOP-989A86S\KKH <checknow@naver.com>
2018-03-10 03:58:54 +0900

    no message

--- StartConsole/Program.cs
+++ StartConsole/Program.cs
@@ -10,50 +10,24 @@ namespace StartConsole
     {
         static void Main(string[] args)
         {
-            //객체 생성
-            Engine.ValuationResult res1 = new Engine.ValuationResult(0.0, 0.1, 0.2, 0.3, 0.4);
-            Engine.ValuationResult res2 = new Engine.ValuationResult(1.0, 1.1, 1.2, 1.3, 1.4);
-            Engine.ValuationResult res3 = new Engine.ValuationResult(2.0, 2.1, 2.2, 2.3, 2.4);
-
-            //테스트1, 객체
-            byte[] resb1 = Serialization.SerializationUtil.SerializeToByte(res1);
-            Engine.ValuationResult res1Returned = (Engine.ValuationResult)Serialization.SerializationUtil.DeserializeToObject(resb1);
-
-            //테스트2, Dic<객체> 
-            Dictionary<int, Engine.ValuationResult> dicRes = new Dictionary<int, Engine.ValuationResult>();
-            dicRes.Add(1, res1);
-            dicRes.Add(2, res2);
-            dicRes.Add(3, res3);
-
-            byte[] dicResByte = Serialization.SerializationUtil.SerializeToByte(dicRes);
-            Dictionary<int, Engine.ValuationResult> dicResReturned = (Dictionary<int, Engine.ValuationResult>)Serialization.SerializationUtil.DeserializeToObject(dicResByte);
-
-            //테스트3, List<List<객체>> 
-            Dictionary<int, Engine.ValuationResult> dicRes2 = new Dictionary<int, Engine.ValuationResult>();
-            dicRes2.Add(1, res1);
-            dicRes2.Add(2, res2);
-            dicRes2.Add(3, res3);
-
-            Dictionary<int, Dictionary<int, Engine.ValuationResult>> dicOfDicRes = new Dictionary<int, Dictionary<int, Engine.ValuationResult>>();
-            dicOfDicRes.Add(1, dicRes);
-            dicOfDicRes.Add(2, dicRes2);
-            
-            byte[] dicOfDicResByte = Serialization.SerializationUtil.SerializeToByte(dicOfDicRes);
-            Dictionary<int, Dictionary<int, Engine.ValuationResult>> dicOfDicResReturned = (Dictionary<int, Dictionary<int, Engine.ValuationResult>>)Serialization.SerializationUtil.DeserializeToObject(dicOfDicResByte);
-
-            Console.WriteLine();
+            SocketTest();            
         }
 
-        static void Main2(string[] args)
+        static void SocketTest()
         {
-            //객체 생성
+            //쓰레드2개 
+            //전송해보기
+        }
+
+        static void StreamTestList()
+        {     //객체 생성
             Engine.ValuationResult res1 = new Engine.ValuationResult(0.0, 0.1, 0.2, 0.3, 0.4);
             Engine.ValuationResult res2 = new Engine.ValuationResult(1.0, 1.1, 1.2, 1.3, 1.4);
             Engine.ValuationResult res3 = new Engine.ValuationResult(2.0, 2.1, 2.2, 2.3, 2.4);
 
             //테스트1, 객체
-            byte[] resb1 = Serialization.SerializationUtil.SerializeToByte(res1);
-            Engine.ValuationResult res1Returned = (Engine.ValuationResult)Serialization.SerializationUtil.DeserializeToObject(resb1);
+            byte[] resb1 = Tools.SerializationUtil.SerializeToByte(res1);
+            Engine.ValuationResult res1Returned = (Engine.ValuationResult)Tools.SerializationUtil.DeserializeToObject(resb1);
 
             //테스트2, List<객체> 
             List<Engine.ValuationResult> listRes = new List<Engine.ValuationResult>();
@@ -61,8 +35,8 @@ namespace StartConsole
             listRes.Add(res2);
             listRes.Add(res3);
 
-            byte[] listResB = Serialization.SerializationUtil.SerializeToByte(listRes);
-            List<Engine.ValuationResult> listResReturned = (List<Engine.ValuationResult>)Serialization.SerializationUtil.DeserializeToObject(listResB);
+            byte[] listResB = Tools.SerializationUtil.SerializeToByte(listRes);
+            List<Engine.ValuationResult> listResReturned = (List<Engine.ValuationResult>)Tools.SerializationUtil.DeserializeToObject(listResB);
 
             //테스트3, List<List<객체>> 
             List<Engine.ValuationResult> listRes2 = new List<Engine.ValuationResult>();
@@ -74,8 +48,43 @@ namespace StartConsole
             listOfListRes.Add(listRes);
             listOfListRes.Add(listRes2);
 
-            byte[] listOfListResB = Serialization.SerializationUtil.SerializeToByte(listOfListRes);
-            List<List<Engine.ValuationResult>> listOfListResReturned = (List<List<Engine.ValuationResult>>)Serialization.SerializationUtil.DeserializeToObject(listOfListResB);
+            byte[] listOfListResB = Tools.SerializationUtil.SerializeToByte(listOfListRes);
+            List<List<Engine.ValuationResult>> listOfListResReturned = (List<List<Engine.ValuationResult>>)Tools.SerializationUtil.DeserializeToObject(listOfListResB);
+
+            Console.WriteLine();
+        }
+
+        static void StreamTestDic()
+        { //객체 생성
+            Engine.ValuationResult res1 = new Engine.ValuationResult(0.0, 0.1, 0.2, 0.3, 0.4);
+            Engine.ValuationResult res2 = new Engine.ValuationResult(1.0, 1.1, 1.2, 1.3, 1.4);
+            Engine.ValuationResult res3 = new Engine.ValuationResult(2.0, 2.1, 2.2, 2.3, 2.4);
+
+            //테스트1, 객체
+            byte[] resb1 = Tools.SerializationUtil.SerializeToByte(res1);
+            Engine.ValuationResult res1Returned = (Engine.ValuationResult)Tools.SerializationUtil.DeserializeToObject(resb1);
+
+            //테스트2, Dic<객체> 
+            Dictionary<int, Engine.ValuationResult> dicRes = new Dictionary<int, Engine.ValuationResult>();
+            dicRes.Add(1, res1);
+            dicRes.Add(2, res2);
+            dicRes.Add(3, res3);
+
+            byte[] dicResByte = Tools.SerializationUtil.SerializeToByte(dicRes);
+            Dictionary<int, Engine.ValuationResult> dicResReturned = (Dictionary<int, Engine.ValuationResult>)Tools.SerializationUtil.DeserializeToObject(dicResByte);
+
+            //테스트3, List<List<객체>> 
+            Dictionary<int, Engine.ValuationResult> dicRes2 = new Dictionary<int, Engine.ValuationResult>();
+            dicRes2.Add(1, res1);
+            dicRes2.Add(2, res2);
+            dicRes2.Add(3, res3);
+
+            Dictionary<int, Dictionary<int, Engine.ValuationResult>> dicOfDicRes = new Dictionary<int, Dictionary<int, Engine.ValuationResult>>();
+            dicOfDicRes.Add(1, dicRes);
+            dicOfDicRes.Add(2, dicRes2);
+
+            byte[] dicOfDicResByte = Tools.SerializationUtil.SerializeToByte(dicOfDicRes);
+            Dictionary<int, Dictionary<int, Engine.ValuationResult>> dicOfDicResReturned = (Dictionary<int, Dictionary<int, Engine.ValuationResult>>)Tools.SerializationUtil.DeserializeToObject(dicOfDicResByte);
 
             Console.WriteLine();
         }
--- Tools/TcpIp.cs
+++ Tools/TcpIp.cs
@@ -1,1 +1,142 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Tools
+{
+    public class SendReceive
+    {
+        public static void send(Socket clientSock, byte[] data)
+        {
+            //글자수 전송
+            int dl = data.Length;
+            byte[] dlb = BitConverter.GetBytes(dl);
+            clientSock.Send(dlb);
+
+            //답변
+            byte[] lb1 = new byte[8196];
+            clientSock.Receive(lb1);
+            //bool lbool1 = BitConverter.ToBoolean(lb1, 0);
+            
+            //메모리전송
+            clientSock.Send(data);
+
+            //답변
+            byte[] lb2 = new byte[8196];
+            clientSock.Receive(lb2);
+            //bool lbool1 = BitConverter.ToBoolean(lb1, 0);
+            //BitConverter.ToInt32(bytes, 0);
+        }
+
+        public static byte[] Receive(Socket clientSock)
+        {
+            //길이 받기
+            byte[] dlb = new byte[8196];
+            clientSock.Receive(dlb);
+            int length = BitConverter.ToInt32(dlb, 0);
+
+            //true 반환
+            clientSock.Send(BitConverter.GetBytes(true));
+
+            //데이터 받기
+            byte[] data = new byte[length];
+            clientSock.Receive(data);
+
+            //결과 반환
+            clientSock.Send(BitConverter.GetBytes(true));
+
+            return data;
+        }        
+    }
+
+    public class ServerSocket
+    {
+        Socket sock;
+        Socket clientSock;
+        
+        public ServerSocket(int port)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // (2) 포트에 바인드
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, port);
+            sock.Bind(ep);
+
+            // (3) 포트 Listening 시작
+            sock.Listen(10);
+
+            // (4) 연결을 받아들여 새 소켓 생성 (하나의 연결만 받아들임)
+            Socket clientSock = sock.Accept();
+
+            //byte[] buff = new byte[8192];
+            //while (!Console.KeyAvailable) // 키 누르면 종료
+            //{
+            //    // (5) 소켓 수신
+            //    int n = clientSock.Receive(buff);
+
+            //    string data = Encoding.UTF8.GetString(buff, 0, n);
+            //    Console.WriteLine(data);
+
+            //    // (6) 소켓 송신
+            //    clientSock.Send(buff, 0, n, SocketFlags.None);  // echo
+            //}
+        }
+
+        public void Close()
+        {
+            // (7) 소켓 닫기
+            clientSock.Close();
+            sock.Close();
+        }
+    }
+
+    public class ClientSocket
+    {
+        Socket sock;
+        
+        public ClientSocket(string serverIP, int port)
+        {
+            // (1) 소켓 객체 생성 (TCP 소켓)
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // (2) 서버에 연결
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(serverIP), port);
+            sock.Connect(ep);
+
+            string cmd = string.Empty;
+            //byte[] receiverBuff = new byte[8192];
+
+            //Console.WriteLine("Connected... Enter Q to exit");
+
+            //// Q 를 누를 때까지 계속 Echo 실행
+            //while ((cmd = Console.ReadLine()) != "Q")
+            //{
+            //    byte[] buff = Encoding.UTF8.GetBytes(cmd);
+
+            //    // (3) 서버에 데이타 전송
+            //    sock.Send(buff, SocketFlags.None);
+
+            //    // (4) 서버에서 데이타 수신
+            //    int n = sock.Receive(receiverBuff);
+
+            //    string data = Encoding.UTF8.GetString(receiverBuff, 0, n);
+            //    Console.WriteLine(data);
+            //}
+
+            // (5) 소켓 닫기
+
+        }
+
+        public void Close()
+        {
+            sock.Close();
+        }
+    }
+
+    
+}
